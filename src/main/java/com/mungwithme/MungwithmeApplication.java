65779d68e27428e453a543580a64953e11d972c1
Jangsuhyeon <shjang0043@gmail.com>
2024-08-08 16:24:47 +0900

    feat: 초기 프로젝트 설정

---
+++ src/main/java/com/mungwithme/MungwithmeApplication.java
@@ -0,0 +1,13 @@
+package com.mungwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungwithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungwithmeApplication.class, args);
+	}
+
+}
