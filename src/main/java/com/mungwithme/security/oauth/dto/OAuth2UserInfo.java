dd663d76a3c3f2bfd685f376706adef3ac999daa
Jangsuhyeon <shjang0043@gmail.com>
2024-09-19 14:53:32 +0900

    [BACK-28] fix: naver oauth2 앱 재설정 및 기능 수정

--- src/main/java/com/mungwithme/security/oauth/dto/OAuth2UserInfo.java
+++ src/main/java/com/mungwithme/security/oauth/dto/OAuth2UserInfo.java
@@ -4,12 +4,14 @@ import com.mungwithme.user.model.Role;
 import com.mungwithme.user.model.SocialType;
 import com.mungwithme.user.model.entity.User;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
 /**
  * 소셜별로 받는 데이터를 분기 처리하는 DTO 클래스
  */
+@Slf4j
 @Builder
 public record OAuth2UserInfo(
     String name,
@@ -19,10 +21,19 @@ public record OAuth2UserInfo(
 ) {
 
     public static OAuth2UserInfo of(String registrationId, Map<String, Object> attributes) {
+        log.info("attributes : {}", attributes);
         return switch (registrationId) { // registration id별로 userInfo 생성
             case "google" -> ofGoogle(attributes);
+            case "naver" -> ofNaver(attributes);
             default -> throw new IllegalStateException("Unexpected value: " + registrationId);
         };
+    }
+
+    private static OAuth2UserInfo ofNaver(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .email((String) ((Map<String, Object>) attributes.get("response")).get("email"))
+                .socialType(SocialType.NAVER)
+                .build();
     }
 
     private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
