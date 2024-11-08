bfd838e707300b733c5a6b30fc73f3e306483652
Jangsuhyeon <shjang0043@gmail.com>
2024-11-08 14:29:05 +0900

    이전 앱 종료하고 jar 실행하도록 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PROJECT_ROOT="/home/ec2-user/deploy"
 JAR_FILE="$PROJECT_ROOT/mungwithme-0.0.1-SNAPSHOT.jar"
 
@@ -5,16 +7,29 @@ APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+TIME_NOW=$(date +"%Y-%m-%d %H:%M:%S")
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT/
-
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-source ~/.bashrc
-nohup sudo java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
+# 1. 이전 애플리케이션 종료
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
+if [ -n "$CURRENT_PID" ]; then
+    echo "$TIME_NOW > 이전 프로세스 종료: PID $CURRENT_PID" >> $DEPLOY_LOG
+    sudo kill -15 "$CURRENT_PID"
+    sleep 5
+fi
+
+# 2. 최신 JAR 파일 복사
+echo "$TIME_NOW > JAR 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# 3. 환경 변수 로드
+echo "$TIME_NOW > 환경 변수 로드" >> $DEPLOY_LOG
+source ~/.bashrc
+
+# 4. JAR 파일 실행
+echo "$TIME_NOW > JAR 파일 실행: $JAR_FILE" >> $DEPLOY_LOG
+nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+# 5. 실행된 프로세스 아이디 출력
+NEW_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디: $NEW_PID" >> $DEPLOY_LOG
