3a71a0b075a8a90000d88cef9c50df4757bc3fea
lhn925 <lim642666@gmail.com>
2025-01-19 19:27:58 +0900

    reboot

--- src/main/java/com/mungwithme/security/SecurityConfig.java
+++ src/main/java/com/mungwithme/security/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://218.239.21.150:5173","https://mungwithme.site"));
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://218.239.21.150:5173",
+                        "https://mungwithme.site"));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
