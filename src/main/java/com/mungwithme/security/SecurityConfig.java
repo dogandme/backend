6e8ea78837933dbbb30d8b2c0b384ad15453ed69
lhn925 <lim642666@gmail.com>
2025-01-26 21:19:47 +0900

    feat : cors 설정 변경

--- src/main/java/com/mungwithme/security/SecurityConfig.java
+++ src/main/java/com/mungwithme/security/SecurityConfig.java
@@ -75,16 +75,14 @@ public class SecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://218.239.21.150:5173",
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
+                        "http://218.239.21.150:5173",
                         "https://mungwithme.site"));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
                     configuration.setMaxAge(3600L);
                     configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-
-                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                    source.registerCorsConfiguration("/**", configuration);
                     return configuration;
                 }
             }));
